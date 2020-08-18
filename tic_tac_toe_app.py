ddd4a2ca22fc83e33b78e8d5a2007f1acdcef74e
Girum-Haile <35455380+Girum-Haile@users.noreply.github.com>
2020-08-19 02:48:32 +0300

    display game board

--- tic_tac_toe_app.py
+++ tic_tac_toe_app.py
@@ -2,3 +2,17 @@
 board = ["-", "-", "-",
          "-", "-", "-",
          "-", "-", "-"]
+
+game_still_going = True
+winner = None
+current_player = "X"
+
+
+# display's the game board
+def display_board():
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print(board[6] + " | " + board[7] + " | " + board[8])
+
+
+
