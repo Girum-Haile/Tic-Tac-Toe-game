c98b5451373757b9ab3e618e272cb344ce6b3edf
Girum-Haile <35455380+Girum-Haile@users.noreply.github.com>
2020-08-19 02:54:52 +0300

    defined function to check possibilites for game win

--- tic_tac_toe_app.py
+++ tic_tac_toe_app.py
@@ -64,3 +64,51 @@ def check_for_winner():  # check for winner
         winner = None
     return
 
+
+def check_rows():
+    global game_still_going
+
+    row1 = board[0] == board[1] == board[2] != '-'
+    row2 = board[3] == board[4] == board[5] != '-'
+    row3 = board[6] == board[7] == board[8] != '-'
+    if row1 or row2 or row3:
+        game_still_going = False
+    # return the winner X or O
+    if row1:
+        return board[0]
+    elif row2:
+        return board[3]
+    elif row3:
+        return board[6]
+    return
+
+
+def check_columns():
+    global game_still_going
+    column1 = board[0] == board[3] == board[6] != '-'
+    column2 = board[1] == board[4] == board[7] != '-'
+    column3 = board[2] == board[5] == board[8] != '-'
+    if column1 or column2 or column3:
+        game_still_going = False
+    # return the winner X or O
+    if column1:
+        return board[0]
+    elif column2:
+        return board[1]
+    elif column3:
+        return board[2]
+    return
+
+
+def check_diagonals():
+    global game_still_going
+    diagonal1 = board[0] == board[4] == board[8] != '-'
+    diagonal2 = board[6] == board[4] == board[2] != '-'
+    if diagonal1 or diagonal2:
+        game_still_going = False
+    # return the winner X or O
+    if diagonal1:
+        return board[0]
+    elif diagonal2:
+        return board[6]
+    return
